5d89fed8b9604667616ccd7ad29c256b485fe20c
alexmaryin <java73@yandex.ru>
2021-10-22 10:27:59 +0400

    Initial commit

---
+++ common/build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.compose.compose
+
+plugins {
+    id("com.android.library")
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+kotlin {
+    android()
+    jvm("desktop")
+
+    sourceSets {
+        named("commonMain") {
+            dependencies {
+                api(compose.runtime)
+                api(compose.foundation)
+                api(compose.material)
+                // Needed only for preview.
+                implementation(compose.preview)
+                // Decompose navigation library
+                implementation("com.arkivanov.decompose:decompose:0.4.0")
+                implementation("com.arkivanov.decompose:extensions-compose-jetbrains:0.4.0")
+            }
+        }
+        named("androidMain") {
+            dependencies {
+                api("androidx.appcompat:appcompat:1.3.1")
+                api("androidx.core:core-ktx:1.6.0")
+            }
+        }
+    }
+}
+
+android {
+    compileSdkVersion(31)
+
+    defaultConfig {
+        minSdkVersion(21)
+        targetSdkVersion(31)
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    sourceSets {
+        named("main") {
+            manifest.srcFile("src/androidMain/AndroidManifest.xml")
+            res.srcDirs("src/androidMain/res")
+        }
+    }
+}
