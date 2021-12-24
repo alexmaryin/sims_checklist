dce258140c8a48a1eb608e48b0541ed198403737
alexmaryin <java73@yandex.ru>
2021-12-24 16:57:14 +0400

    METAR parser almost ready.

--- common/src/commonMain/kotlin/feature/metarParser/Metar.kt
+++ common/src/commonMain/kotlin/feature/metarParser/Metar.kt
@@ -9,7 +9,13 @@ data class Metar(
     val station: String?,
     val reportTime: LocalDateTime?,
     val wind: Wind?,
-)
+    val visibility: Visibility?,
+    val phenomenons: Phenomenons?,
+    val clouds: List<CloudLayer>,
+    val temperature: Temperature?
+) {
+    val ceilingAndVisibilityOK get() = clouds.isEmpty() && visibility?.distAll == 9999 && phenomenons != null
+}
 
 data class Wind(
     val direction: Int = 0,
@@ -17,17 +23,71 @@ data class Wind(
     val speed: Int = 0,
     val speedUnits: WindUnit = WindUnit.KT,
     val gusts: Int = 0
+) {
+    val isCalm get() = speed == 0 && direction == 0
+}
+
+data class Visibility(
+    val distAll: Int? = null,
+    val byDirections: List<VisibilityByDir> = emptyList(),
+    val byRunways: List<VisibilityByRunway> = emptyList(),
+    val distUnits: VisibilityUnit = VisibilityUnit.METERS,
+    )
+
+data class VisibilityByDir(
+    val dist: Int,
+    val direction: VisibilityDirection
+)
+
+data class VisibilityByRunway(
+    val dist: Int,
+    val runway: String
+)
+
+data class Phenomenons(
+    val all: Set<WeatherPhenomenons> = emptySet(),
+    val intensity: PhenomenonIntensity = PhenomenonIntensity.NONE
+)
+
+data class CloudLayer(
+    val type: CloudsType,
+    val lowMarginFt: Int,
+    val cumulusType: CumulusType? = null
+)
+
+data class Temperature(
+    val air: Int,
+    val dewPoint: Int
 )
 
 enum class WindUnit { KT, MPS, KPH }
 
+enum class VisibilityUnit { METERS, SM }
+
+enum class VisibilityDirection { NORTH, NORTH_EAST, EAST, SOUTH_EAST, SOUTH, SOUTH_WEST, WEST, NORTH_WEST }
+
+enum class PhenomenonIntensity { NONE, INCREASING, DECREASING }
+
+enum class WeatherPhenomenons(val code: String) {
+    DRIZZLE("DZ"), RAIN("RA"), SNOW("SN"), SNOW_GRAINS("SG"), ICE_PELLETS("PL"), SMALL_HAIL("GS"),
+    HAIL("GR"), SHOWER("SH"), FREEZE("FZ"), THUNDERSTORM("TS"), DUST_STORM("DS"), SANDSTORM("SS"),
+    FOG("FG"), IN_VICINITY("VC"), SHALLOW("MI"), PARTIAL("PR"), PATCHES("BC"), MIST("BR"),
+    HAZE("HZ"), SMOKE("FU"), DUST("DU"), BLOWING("BL"), SQUALL("SQ"), ICE_CRYSTALS("IC"),
+    VOLCANIC_ASH("VA"), DRIFTING("DR"), SAND("SA")
+}
+
+enum class CloudsType(val code: String) {
+    CLEAR("SKC"), NIL_SIGNIFICANT("NSC"), FEW("FEW"), SCATTERED("SCT"), BROKEN("BKN"), OVERCAST("OVC")
+}
+
+enum class CumulusType { CUMULONIMBUS, TOWERING_CUMULUS }
+
 class MetarParser(raw: String) {
-    private val parts = raw.split(" ", "\t").toMutableList()
+    private val parts = raw.split(" ", "\t")
 
     private fun parseStation(): String? {
-        parts.forEachIndexed { index, part ->
+        parts.forEach { part ->
             MetarGroups.STATION.find(part)?.let {
-                parts.removeAt(index)
                 return it.value
             }
         }
@@ -35,9 +95,8 @@ class MetarParser(raw: String) {
     }
 
     private fun parseReportTime(): LocalDateTime? {
-        parts.forEachIndexed { index, part ->
+        parts.forEach { part ->
             MetarGroups.REPORT_TIME.find(part)?.let {
-                parts.removeAt(index)
                 val stamp = Clock.System.now().toLocalDateTime(TimeZone.UTC)
                 return LocalDateTime(
                     stamp.year, stamp.month, it.groupValues[1].toInt(), it.groupValues[2].toInt(), it.groupValues[3].toInt()
@@ -48,19 +107,119 @@ class MetarParser(raw: String) {
     }
 
     private fun parseWind(): Wind? {
-        parts.forEachIndexed { index, part ->
+        parts.forEach { part ->
             MetarGroups.WIND.find(part)?.let {
-                parts.removeAt(index)
                 return Wind(
                     direction = it.groupValues[1].toIntOrNull() ?: 0,
                     variable = it.groupValues[1] == "VRB",
                     speed = it.groupValues[2].toIntOrNull() ?: 0,
-                    speedUnits = when(it.groupValues[4]) {
+                    speedUnits = when (it.groupValues[4]) {
                         "MPS" -> WindUnit.MPS
                         "KPH" -> WindUnit.KPH
                         else -> WindUnit.KT
                     },
                     gusts = it.groupValues[3].toIntOrNull() ?: 0
+                )
+            }
+        }
+        return null
+    }
+
+    private fun parseVisibility(): Visibility? {
+
+        val byDirs: MutableList<VisibilityByDir> = mutableListOf()
+        val byRunways: MutableList<VisibilityByRunway> = mutableListOf()
+
+        parts.forEach { part ->
+            MetarGroups.VISIBILITY.find(part)?.let {
+                when {
+                    it.groupValues[2].isBlank() && it.groupValues[6].isBlank() -> {
+                        return Visibility(
+                            distAll = it.groupValues[1].toIntOrNull() ?: it.groupValues[3].toIntOrNull()
+                            ?: if (it.groupValues[5] == "CAVOK") 9999 else throw RuntimeException("Visibility undefined in part ${it.groupValues[0]}!"),
+                            distUnits = if (it.groupValues[4] == "SM") VisibilityUnit.SM else VisibilityUnit.METERS,
+                        )
+                    }
+                    it.groupValues[2].isNotBlank() -> {
+                        byDirs += VisibilityByDir(
+                            dist = it.groupValues[1].toIntOrNull() ?: throw RuntimeException("Visibility for direction ${it.groupValues[2]} undefined!"),
+                            direction = when (it.groupValues[2]) {
+                                "N" -> VisibilityDirection.NORTH
+                                "NE" -> VisibilityDirection.NORTH_EAST
+                                "E" -> VisibilityDirection.EAST
+                                "SE" -> VisibilityDirection.SOUTH_EAST
+                                "S" -> VisibilityDirection.SOUTH
+                                "SW" -> VisibilityDirection.SOUTH_WEST
+                                "W" -> VisibilityDirection.WEST
+                                "NW" -> VisibilityDirection.NORTH_WEST
+                                else -> throw RuntimeException("Unknown visibility direction ${it.groupValues[2]}!")
+                            }
+                        )
+                    }
+                    it.groupValues[6].isNotBlank() -> {
+                        byRunways += VisibilityByRunway(
+                            dist = it.groupValues[7].toIntOrNull() ?: throw RuntimeException("Visibility for runway ${it.groupValues[6]} undefined!"),
+                            runway = it.groupValues[6]
+                        )
+                    }
+                }
+            }
+        }
+        return if (byDirs.isNotEmpty() || byRunways.isNotEmpty()) Visibility(byDirections = byDirs, byRunways = byRunways) else null
+    }
+
+    private fun parsePhenomenons(): Phenomenons? {
+
+        val items = mutableSetOf<WeatherPhenomenons>()
+        var intensity = PhenomenonIntensity.NONE
+
+        parts.forEach { part ->
+            MetarGroups.PHENOMENONS.find(part)?.let { match ->
+                if (match.groupValues[1].isNotBlank()) {
+                    intensity = when(match.groupValues[1]) {
+                        "+" -> PhenomenonIntensity.INCREASING
+                        "-" -> PhenomenonIntensity.DECREASING
+                        else -> PhenomenonIntensity.NONE
+                    }
+                }
+                if(match.groupValues[2].isNotBlank()) {
+                    match.groupValues[2].windowed(2, 2) { code ->
+                        items += WeatherPhenomenons.values().first { it.code == code }
+                    }
+                }
+            }
+        }
+        return if(items.isNotEmpty()) Phenomenons(items, intensity) else null
+    }
+
+    private fun parseClouds(): List<CloudLayer> {
+
+        val items = mutableListOf<CloudLayer>()
+
+        parts.forEach { part ->
+            MetarGroups.CLOUDS.find(part)?.let { match ->
+                val type = CloudsType.values().first { it.code == match.groupValues[1] }
+                val cumulus = when(match.groupValues[3]) {
+                    "CB" -> CumulusType.CUMULONIMBUS
+                    "TCU" -> CumulusType.TOWERING_CUMULUS
+                    else -> null
+                }
+                items += CloudLayer(
+                    type = type,
+                    lowMarginFt = match.groupValues[2].toIntOrNull() ?: throw RuntimeException("No margin for cloud layer in ${match.groupValues[0]}"),
+                    cumulusType =  cumulus
+                )
+            }
+        }
+        return items.sortedBy { it.lowMarginFt }
+    }
+
+    private fun parseTemperature(): Temperature? {
+        parts.forEach { part ->
+            MetarGroups.TEMPERATURE_DEW.find(part)?.let { match ->
+                return Temperature(
+                    air = match.groupValues[1].replace("M", "-").toInt(),
+                    dewPoint = match.groupValues[2].replace("M", "-").toInt()
                 )
             }
         }
@@ -73,6 +232,10 @@ class MetarParser(raw: String) {
             station = parseStation(),
             reportTime = parseReportTime(),
             wind = parseWind(),
+            visibility = parseVisibility(),
+            phenomenons = parsePhenomenons(),
+            clouds = parseClouds(),
+            temperature = parseTemperature(),
         )
     }
 }
--- common/src/commonTest/kotlin/featureTest/metarParser/RegexesTest.kt
+++ common/src/commonTest/kotlin/featureTest/metarParser/RegexesTest.kt
@@ -1,8 +1,6 @@
 package featureTest.metarParser
 
-import feature.metarParser.MetarParser
-import feature.metarParser.Wind
-import feature.metarParser.WindUnit
+import feature.metarParser.*
 import kotlinx.datetime.LocalDateTime
 import kotlin.test.Test
 import kotlin.test.assertTrue
@@ -13,9 +11,10 @@ internal class RegexesTest {
         UWLL 231100Z 20002MPS 9999 BKN030 M22/M26 Q1012 R20/820245 NOSIG RMK QFE749/0999
         KLAX 231053Z 35005KT 10SM FEW006 BKN040 OVC070 14/11 A3003 RMK AO2 RAB05E53 SLP168 P0000 T01390111
         LOWI 231120Z VRB02KT CAVOK 00/M07 Q1018 NOSIG
-        ULLI 231100Z 24007G12MPS 200V290 9000 -SHSN DRSN BKN014CB OVC032 M03/M06 Q0998 RESHSN R28L/491037 R28R/451040 TEMPO 22010G16MPS 1000 SHSN BLSN BKN007 BKN020CB
+        ULLI 231100Z 24007G12MPS 200V290 9000 -SHSN DRSN BKN014CB OVC032 M03/M06 Q0998 RESHSN R28L/491037 R28R/451040
         UWWW 231100Z VRB01MPS CAVOK M20/M25 Q1012 R33/450250 NOSIG RMK QFE748/0998
         EGLL 231120Z AUTO 20009KT 6000 OVC004 08/07 Q1008 TEMPO BKN005
+        UEEE 161500Z 00000MPS 0150NE 0250NW R23L/0450 FG VV003 M57/M60 Q1038 NOSIG RMK QBB090 QFE770 23450245
     """.trimIndent().split("\n")
 
     @Test
@@ -23,7 +22,7 @@ internal class RegexesTest {
         val stations = metarExamples.mapNotNull { raw ->
             MetarParser(raw).parse().station
         }
-        assertTrue { stations == listOf("UWLL", "KLAX", "LOWI", "ULLI", "UWWW", "EGLL") }
+        assertTrue { stations == listOf("UWLL", "KLAX", "LOWI", "ULLI", "UWWW", "EGLL", "UEEE") }
     }
 
     @Test
@@ -31,14 +30,17 @@ internal class RegexesTest {
         val reports = metarExamples.mapNotNull { raw ->
             MetarParser(raw).parse().reportTime
         }
-        assertTrue { reports == listOf(
-            LocalDateTime.parse("2021-12-23T11:00:00"),
-            LocalDateTime.parse("2021-12-23T10:53:00"),
-            LocalDateTime.parse("2021-12-23T11:20:00"),
-            LocalDateTime.parse("2021-12-23T11:00:00"),
-            LocalDateTime.parse("2021-12-23T11:00:00"),
-            LocalDateTime.parse("2021-12-23T11:20:00"),
-        ) }
+        assertTrue {
+            reports == listOf(
+                LocalDateTime.parse("2021-12-23T11:00:00"),
+                LocalDateTime.parse("2021-12-23T10:53:00"),
+                LocalDateTime.parse("2021-12-23T11:20:00"),
+                LocalDateTime.parse("2021-12-23T11:00:00"),
+                LocalDateTime.parse("2021-12-23T11:00:00"),
+                LocalDateTime.parse("2021-12-23T11:20:00"),
+                LocalDateTime.parse("2021-12-16T15:00:00"),
+            )
+        }
     }
 
     @Test
@@ -46,13 +48,101 @@ internal class RegexesTest {
         val winds = metarExamples.mapNotNull { raw ->
             MetarParser(raw).parse().wind
         }
-        assertTrue { winds == listOf(
-            Wind(direction = 200, speed = 2, speedUnits = WindUnit.MPS),
-            Wind(direction = 350, speed = 5, speedUnits = WindUnit.KT),
-            Wind(variable = true, speed = 2, speedUnits = WindUnit.KT),
-            Wind(direction = 240, speed = 7, speedUnits = WindUnit.MPS, gusts = 12),
-            Wind(variable = true, speed = 1, speedUnits = WindUnit.MPS),
-            Wind(direction = 200, speed = 9, speedUnits = WindUnit.KT),
-        ) }
+        assertTrue {
+            winds == listOf(
+                Wind(direction = 200, speed = 2, speedUnits = WindUnit.MPS),
+                Wind(direction = 350, speed = 5, speedUnits = WindUnit.KT),
+                Wind(variable = true, speed = 2, speedUnits = WindUnit.KT),
+                Wind(direction = 240, speed = 7, speedUnits = WindUnit.MPS, gusts = 12),
+                Wind(variable = true, speed = 1, speedUnits = WindUnit.MPS),
+                Wind(direction = 200, speed = 9, speedUnits = WindUnit.KT),
+                Wind(direction = 0, speed = 0, speedUnits = WindUnit.MPS),
+            )
+        }
+    }
+
+    @Test
+    fun `Parser should find correct visibility information`() {
+        val visibility = metarExamples.mapNotNull { raw ->
+            MetarParser(raw).parse().visibility
+        }
+        assertTrue {
+            visibility == listOf(
+                Visibility(distAll = 9999),
+                Visibility(distAll = 10, distUnits = VisibilityUnit.SM),
+                Visibility(distAll = 9999),
+                Visibility(distAll = 9000),
+                Visibility(distAll = 9999),
+                Visibility(distAll = 6000),
+                Visibility(
+                    byDirections = listOf(
+                        VisibilityByDir(dist = 150, direction = VisibilityDirection.NORTH_EAST),
+                        VisibilityByDir(dist = 250, direction = VisibilityDirection.NORTH_WEST),
+                    ),
+                    byRunways = listOf(
+                        VisibilityByRunway(dist = 450, runway = "23L")
+                    )
+                )
+            )
+        }
+    }
+
+    @Test
+    fun `Parses should find correct phenomenon information`() {
+        val phenomenons = metarExamples.mapNotNull { raw ->
+            MetarParser(raw).parse().phenomenons
+        }
+        assertTrue {
+            phenomenons == listOf(
+                Phenomenons(all = setOf(WeatherPhenomenons.SHOWER, WeatherPhenomenons.SNOW, WeatherPhenomenons.DRIFTING),
+                    intensity = PhenomenonIntensity.DECREASING),
+                Phenomenons(all = setOf(WeatherPhenomenons.FOG))
+            )
+        }
+    }
+
+    @Test
+    fun `Parses should find correct cloud layers information`() {
+        val clouds = metarExamples.map { raw ->
+            MetarParser(raw).parse().clouds
+        }
+        assertTrue {
+            clouds.filterNot { it.isEmpty() } == listOf(
+                listOf(
+                    CloudLayer(CloudsType.BROKEN, 30)
+                ),
+                listOf(
+                    CloudLayer(CloudsType.FEW, 6),
+                    CloudLayer(CloudsType.BROKEN, 40),
+                    CloudLayer(CloudsType.OVERCAST, 70)
+                ),
+                listOf(
+                    CloudLayer(CloudsType.BROKEN, 14, CumulusType.CUMULONIMBUS),
+                    CloudLayer(CloudsType.OVERCAST, 32)
+                ),
+                listOf(
+                    CloudLayer(CloudsType.OVERCAST, 4),
+                    CloudLayer(CloudsType.BROKEN, 5)
+                )
+            )
+        }
+    }
+
+    @Test
+    fun `Parses should find temperature information`() {
+        val temperature = metarExamples.mapNotNull { raw ->
+            MetarParser(raw).parse().temperature
+        }
+        assertTrue {
+            temperature == listOf(
+                Temperature(-22, -26),
+                Temperature(14, 11),
+                Temperature(0, -7),
+                Temperature(-3, -6),
+                Temperature(-20, -25),
+                Temperature(8, 7),
+                Temperature(-57, -60),
+            )
+        }
     }
 }
